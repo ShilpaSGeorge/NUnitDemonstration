27f4e2c57717b749136ab9b04f562af0d4beb58f
Shilpa Sosa George <shilpasosageorge@gmail.com>
2024-02-19 19:18:41 -0500

    Comments written.

--- TestClass/Class1.cs
+++ TestClass/Class1.cs
@@ -19,12 +19,13 @@ namespace TestClass
         public void ValidTriangle60() 
             
          {
+                //Arrange
                 int firstAngle = 60;
                 int secondAngle = 60;
                 int thirdAngle = 60;
                 
                 string Expected = "The triangle is valid.";
-
+                //Act
                 string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
 
                 //Assert
